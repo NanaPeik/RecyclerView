c950bec17d7648c366a035d3af225d6790362dd1
Nana Peikrishvili <nana.feiqrishvili2014@gmail.com>
2021-03-02 16:40:49 +0400

    added String generator function

--- app/src/main/java/ge/tsu/android/myapplication/recycle/RecyclerViewActivity.kt
+++ app/src/main/java/ge/tsu/android/myapplication/recycle/RecyclerViewActivity.kt
@@ -1,10 +1,14 @@
 package ge.tsu.android.myapplication.recycle
 
 import android.os.Bundle
+import android.util.Log
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import ge.tsu.android.myapplication.R
+import kotlin.random.Random
+import kotlin.random.nextUBytes
 
 class RecyclerViewActivity : AppCompatActivity() {
     lateinit var listsRecyclerView: RecyclerView
@@ -18,5 +22,16 @@ class RecyclerViewActivity : AppCompatActivity() {
         listsRecyclerView.adapter= ListSelectionRecyclerViewAdapter()
 
     }
-
+    fun addItemInListView(view: View): String {
+        var random = Random
+        var finalString = ""
+        var range = random.nextInt(10,20)
+        var item = 0
+        while (item < range){
+            var char = random.nextInt(97,122).toChar()
+            finalString += char
+            item++
+        }
+        return finalString
+    }
 }
