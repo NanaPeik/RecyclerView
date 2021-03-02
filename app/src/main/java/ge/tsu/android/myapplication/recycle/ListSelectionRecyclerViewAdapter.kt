1b0cbae18c8824b6a865f71d69bf66da8601d90d
Nana Peikrishvili <nana.feiqrishvili2014@gmail.com>
2021-03-02 16:40:23 +0400

    added String generator function

--- app/src/main/java/ge/tsu/android/myapplication/recycle/ListSelectionRecyclerViewAdapter.kt
+++ app/src/main/java/ge/tsu/android/myapplication/recycle/ListSelectionRecyclerViewAdapter.kt
@@ -1,15 +1,17 @@
 package ge.tsu.android.myapplication.recycle
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import ge.tsu.android.myapplication.R
 
 class ListSelectionRecyclerViewAdapter:
+    RecyclerView.Adapter<ListSelectionViewHolder>(){
 
-RecyclerView.Adapter<ListSelectionViewHolder>(){
     val listTitles = arrayOf("Shopping List", "Chores", "Android Tutorials")
-
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListSelectionViewHolder {
         val view = LayoutInflater.from(parent.context)
@@ -17,9 +19,8 @@ RecyclerView.Adapter<ListSelectionViewHolder>(){
         return ListSelectionViewHolder(view)
     }
 
-    override fun getItemCount(): Int {
-        return listTitles.size
-    }
+    override fun getItemCount() = listTitles.size
+
 
     override fun onBindViewHolder(holder: ListSelectionViewHolder, position: Int) {
         holder.listPosition.text=(position+1).toString()
