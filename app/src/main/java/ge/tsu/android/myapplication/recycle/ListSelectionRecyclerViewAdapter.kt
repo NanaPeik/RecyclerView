360811463beecb58f3cb6b10afb0ee3833d7d41d
Nana Peikrishvili <nana.feiqrishvili2014@gmail.com>
2021-03-02 16:52:54 +0400

    added constructor

--- app/src/main/java/ge/tsu/android/myapplication/recycle/ListSelectionRecyclerViewAdapter.kt
+++ app/src/main/java/ge/tsu/android/myapplication/recycle/ListSelectionRecyclerViewAdapter.kt
@@ -1,26 +1,20 @@
 package ge.tsu.android.myapplication.recycle
 
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 import ge.tsu.android.myapplication.R
 
-class ListSelectionRecyclerViewAdapter:
+class ListSelectionRecyclerViewAdapter(private val listTitles: Array<String>):
     RecyclerView.Adapter<ListSelectionViewHolder>(){
-
-    val listTitles = arrayOf("Shopping List", "Chores", "Android Tutorials")
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListSelectionViewHolder {
         val view = LayoutInflater.from(parent.context)
-            .inflate(R.layout.list_selection_view_holder,parent,false)
+            .inflate(R.layout.list_selection_view_holder, parent,false)
         return ListSelectionViewHolder(view)
     }
 
     override fun getItemCount() = listTitles.size
-
 
     override fun onBindViewHolder(holder: ListSelectionViewHolder, position: Int) {
         holder.listPosition.text=(position+1).toString()
