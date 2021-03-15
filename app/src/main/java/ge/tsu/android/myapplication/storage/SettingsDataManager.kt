962901ce15093e001c13aa0cbfc192b638c52a0e
Nana Peikrishvili <nana.feiqrishvili2014@gmail.com>
2021-03-15 15:09:53 +0400

    remove Gson

--- app/src/main/java/ge/tsu/android/myapplication/storage/SettingsDataManager.kt
+++ app/src/main/java/ge/tsu/android/myapplication/storage/SettingsDataManager.kt
@@ -2,21 +2,18 @@ package ge.tsu.android.myapplication.storage
 
 import android.content.Context
 import androidx.preference.PreferenceManager
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
+
 
 class SettingsDataManager(private val context: Context) {
-    val gson = Gson()
     fun addSettings(key: String, isChecked: Boolean) {
         val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context).edit()
-        sharedPreferences.putString(key, gson.toJson(isChecked))
+        sharedPreferences.putString(key, isChecked.toString())
         sharedPreferences.apply()
     }
 
     fun readSettingsData(key: String): Boolean {
         val sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context)
         val data = sharedPreferences.getString(key, "")
-        val type = object : TypeToken<Boolean>() {}.getType()
-        return gson.fromJson(data, type)
+        return data.toBoolean()
     }
 }
