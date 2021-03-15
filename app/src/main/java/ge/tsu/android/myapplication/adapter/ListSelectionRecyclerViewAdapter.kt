53534d2d46247e686a32fa12656dfcb0a6adfc41
Nana Peikrishvili <nana.feiqrishvili2014@gmail.com>
2021-03-15 18:52:18 +0400

    onAttachedToRecyclerView

--- app/src/main/java/ge/tsu/android/myapplication/adapter/ListSelectionRecyclerViewAdapter.kt
+++ app/src/main/java/ge/tsu/android/myapplication/adapter/ListSelectionRecyclerViewAdapter.kt
@@ -1,5 +1,6 @@
 package ge.tsu.android.myapplication.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,6 +9,7 @@ import androidx.recyclerview.widget.RecyclerView
 import ge.tsu.android.myapplication.Activity.RecyclerViewActivity
 import ge.tsu.android.myapplication.R
 import ge.tsu.android.myapplication.data.RecycleViewItem
+import ge.tsu.android.myapplication.storage.ListDataManager
 import java.util.*
 import kotlin.collections.ArrayList
 
@@ -23,24 +25,40 @@ class ListSelectionRecyclerViewAdapter(
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListSelectionViewHolder {
         val view = LayoutInflater.from(parent.context)
             .inflate(R.layout.item_view_recycle, parent, false)
+
         return ListSelectionViewHolder(view)
     }
 
-    override fun getItemCount() = filteredList.size
+    override fun getItemCount() = listTitles.size
+
+    lateinit var context: Context
+    override fun onAttachedToRecyclerView(recyclerView: RecyclerView) {
+        super.onAttachedToRecyclerView(recyclerView)
+        context = recyclerView.context
+
+    }
+    val listDataManager = ListDataManager(context)
 
     override fun onBindViewHolder(holder: ListSelectionViewHolder, position: Int) {
         holder.listPosition.text = filteredList.get(position).itemNumber.toString()
         holder.listTitle.text = filteredList[position].itemText
-        holder.listCheck.isChecked = filteredList.get(position).isChecked
+        holder.listCheck.isChecked = filteredList[position].isChecked
         holder.listItemDetiles.text = filteredList.get(position).detiles
 
+
         holder.listCheck.setOnCheckedChangeListener { compoundButton: CompoundButton, b: Boolean ->
-            if (!RecyclerViewActivity.showChecked && b) {
+            holder.listCheck.isChecked = b
+            listTitles.get(position).isChecked=b
+            listDataManager.add(listTitles)
+            if (!RecyclerViewActivity.showChecked && !b) {
                 filteredList.removeAt(position)
                 notifyItemRemoved(position)
             }
+
         }
+
     }
+
 
     class ListSelectionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val listPosition = itemView.findViewById(R.id.itemNumber) as TextView
